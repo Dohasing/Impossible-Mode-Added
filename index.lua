be5b512ab0560ade04f9adf21e2bbeec6a0c8c51
Dohasing <9221597740i@gmail.com>
2025-05-02 12:57:04 +0500

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,11 +1,9 @@
 wait(2)
-game.Workspace.CurrentRooms["0"].Door.Door.Changed:Connect(function ()
-    if game.Workspace.CurrentRooms["0"].Door.Door.CanCollide == false then
+game.Workspace.CurrentRooms["0"].Door.Func_Open.Event:Connect(function ()
         print("Ol yee baby!!!")
         game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption.Text = "Impossible Mode Added"
         game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption.Visible = true
         game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption.TextTransparency = 0
         wait(2)
         game:GetService("TweenService"):Create(game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption, TweenInfo.new(7), {TextTransparency = 1}):Play()
-    end
 end)
